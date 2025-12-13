a09d94a99f5ea60487628bcc8f95d9e3a2d9d6e7
slabwu <134193743+slabwu@users.noreply.github.com>
2025-12-13 20:30:17 +0800

    Edit deserializeUser()

--- config/passport.js
+++ config/passport.js
@@ -28,6 +28,11 @@ function initialisePassport(pool) {
     passport.deserializeUser(async (id, done) => {
         try {
             const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+            
+            if (!rows.length) {
+                return done(null, false)
+            }
+
             const user = rows[0]
             done(null, user)
         } catch (err) {
