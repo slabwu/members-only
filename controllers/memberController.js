208754cba7b5edc465304234f49e95319f7e30ba
slabwu <134193743+slabwu@users.noreply.github.com>
2026-01-02 21:54:05 +0800

    Edit membership controller

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -9,10 +9,11 @@ exports.postMembership = async (req, res) => {
     switch(req.body.answer) {
         case process.env.MEMBER:
             await db.grantMember(req.user.id)
-            res.redirect('/posts')
+            return res.redirect('/posts')
         case process.env.ADMIN:
+            await db.grantMember(req.user.id)
             await db.grantAdmin(req.user.id)
-            res.redirect('/posts')
+            return res.redirect('/posts')
         default:
             req.flash('error', 'Incorrect!')
             res.redirect('/membership')
